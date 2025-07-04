62541f445973b8fa8c489e32667952726559e4ca
inderkukreja17 <inderkukreja2@gmail.com>
2025-07-04 10:54:03 +0530

    Added Day 6 to Day 9 projects

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,8 @@
+def my_function():
+    print("hello")
+    print("bye")
+
+
+my_function()
+
+# reeborg projects done on website
---
+++ Day 7/task/main.py
@@ -0,0 +1,118 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel", "banana"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+# Todo 1
+print(logo)
+lives = 6
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+
+for letter in chosen_word:
+    placeholder += "_"
+
+print(placeholder)
+
+# Todo 2
+
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+    guess = input("Guess a letter:").lower()
+
+    display = " "
+    # Todo 3
+    for letter in chosen_word:
+
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over == True
+            print("you loose")
+
+    if "_" not in display:
+        game_over = True
+        print("You win")
+
+    print(stages[lives])
+
